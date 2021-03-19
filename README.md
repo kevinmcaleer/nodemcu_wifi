468d8c61b70d92e88720d4a21183427cfee0a3d8
Kevin McAleer <kevinmcaleer@gmail.com>
2021-03-19 21:14:42 +0000

    MQTT Client for MicroPython

---
+++ README.md
@@ -0,0 +1,10 @@
+# MQTT client for MicroPython on the ESP8266
+
+## How to load
+Load the boot.py, main.py and umqttsimply.py onto the ESP8366 - Thonny is a good tool to load this
+
+## Setup the Wifi
+Change the wifi SSID and password in boot.py
+
+## Change the topic
+Change the topic to publish or subscribe to different topics
---
+++ boot.py
@@ -0,0 +1,33 @@
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
+
+ssid = 'wifi name'
+password = 'password'
+mqtt_server = 'server'
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = b'notification'
+topic_pub = b'hello'
+
+last_message = 0
+message_interval = 5
+counter = 0
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+    pass
+
+print('Connection successful')
+print(station.ifconfig())
+
